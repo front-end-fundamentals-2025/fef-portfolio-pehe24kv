a90b229bb1537a11c3b14697e0de3d634c2a04e5
pehe24kv <pehe24kv@student.ju.se>
2025-02-21 13:35:22 +0100

    Updated values
    Updated and changed the measurements to match the website better.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const main = document.getElementById("main");
 
 main.addEventListener("click", function (event) {
-  if (main.style.width == "500px") {
-    main.style.width = "100px";
+  if (main.style.width == "40%") {
+    main.style.width = "20%";
   } else {
-    main.style.width = "500px";
+    main.style.width = "40%";
   }
 });
